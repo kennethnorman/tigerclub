cba4d73713a5d10ae2beac575f5c12e6573de1c8
kennethnorman <norman.kenneth@gmail.com>
2015-01-25 11:01:11 +0000

    Fix diary

--- tigerclub-diary.js
+++ tigerclub-diary.js
@@ -9,7 +9,10 @@ $("tc-allevents").text("Working...");
 
 pagemessage.innerText = "Working...";
 
-$(document).ready(UpdateCalendarEvents("https://www.google.com/calendar/feeds/5hstrrs3i19g9untj4h7tag7d0%40group.calendar.google.com/public/full?singleevents=true&futureevents=true&orderby=starttime&sortorder=ascending&alt=json"));
+$(document).ready(UpdateCalendarEvents(
+//"https://www.google.com/calendar/feeds/5hstrrs3i19g9untj4h7tag7d0%40group.calendar.google.com/public/full?singleevents=true&futureevents=true&orderby=starttime&sortorder=ascending&alt=json"
+"https://www.google.com/calendar/feeds/5hstrrs3i19g9untj4h7tag7d0%40group.calendar.google.com/public/basic?singleevents=true&futureevents=true&orderby=starttime&sortorder=ascending&alt=json"
+));
 
 
 /**/
@@ -26,20 +29,30 @@ function UpdateCalendarEvents(sourceFile) {
 
             var entry = $(el),
                 title = entry[0].title.$t,
-                when = new Date(entry[0].gd$when[0].startTime);
+                summary = entry[0].summary.$t;
+			var when = new Date("1990-01-01");
+			
+			// Format is "When: Sat 12 Sep 2015<br>\n\n\n<br>Event Status: confirmed"
+			var sum = summary.split("<br>");
+			for (var sumIndex= 0; sumIndex<sum.length; sumIndex++) {
+				var entry= sum[sumIndex].split(":");
+				if (entry[0] == "When") {
+					when = new Date(entry[1].trim());
+				}
+			}
 
             foundevents = true;
 
             if (nextevent) {
                 if (i === 0) {
-                    nextevent.appendChild(document.createTextNode(when.toLocaleDateString() + " : "));
+                    nextevent.appendChild(document.createTextNode(when.toLocaleDateString("en-GB") + " : "));
                     nextevent.appendChild(document.createTextNode(title));
                     nextevent.appendChild(document.createElement("br"));
                 }
             }
 
             if (allevents) {
-                allevents.appendChild(document.createTextNode(when.toLocaleDateString() + " : "));
+                allevents.appendChild(document.createTextNode(when.toLocaleDateString("en-GB") + " : "));
                 allevents.appendChild(document.createTextNode(title));
                 allevents.appendChild(document.createElement("br"));
                 allevents.appendChild(document.createElement("br"));
