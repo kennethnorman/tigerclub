98e1d00260b2c82b1f4064a715853b6ddbf120a1
kennethnorman <norman.kenneth@gmail.com>
2015-10-16 16:23:24 +0100

    Using google api for diary

--- tigerclub-diary.js
+++ tigerclub-diary.js
@@ -1,68 +1,113 @@
 // tigerclub-diary.js
 
-var nextevent = document.getElementById("tc-nextevent");
-var allevents = document.getElementById("tc-allevents");
-var pagemessage = document.getElementById("tc-pagemessage");
-var foundevents = false;
-$("tc-nextevent").text("Working...");
-$("tc-allevents").text("Working...");
+	/*var pm = document.getElementById('tc-pagemessage');
+	if (pm) {
+		pm.innerText = 'Working...';
+	}*/
 
-pagemessage.innerText = "Working...";
+      // Your Client ID can be retrieved from your project in the Google
+      // Developer Console, https://console.developers.google.com
+      var CLIENT_ID = '1040191233193-jf54albj9gi8s70up20i27l36o393deb.apps.googleusercontent.com';
 
-$(document).ready(UpdateCalendarEvents(
-//"https://www.google.com/calendar/feeds/5hstrrs3i19g9untj4h7tag7d0%40group.calendar.google.com/public/full?singleevents=true&futureevents=true&orderby=starttime&sortorder=ascending&alt=json"
-"https://www.google.com/calendar/feeds/5hstrrs3i19g9untj4h7tag7d0%40group.calendar.google.com/public/basic?singleevents=true&futureevents=true&orderby=starttime&sortorder=ascending&alt=json"
-));
+      var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
 
+      /**
+       * Check if current user has authorized this application.
+       */
+      function checkAuth() {
+        gapi.auth.authorize(
+          {
+            'client_id': CLIENT_ID,
+            'scope': SCOPES.join(' '),
+            'immediate': true
+          }, handleAuthResult);
+      }
 
-/**/
-function UpdateCalendarEvents(sourceFile) {
-
-    $.getJSON(sourceFile, function (data) {
-
-        if (data.feed.entry.length != 0) {
-            pagemessage.innerText = "";
-            $("tc-pagemessage").hide();
+      /**
+       * Handle response from authorization server.
+       *
+       * @param {Object} authResult Authorization result.
+       */
+      function handleAuthResult(authResult) {
+        var authorizeDiv = document.getElementById('authorize-div');
+        if (authResult && !authResult.error) {
+          // Hide auth UI, then load client library.
+          authorizeDiv.style.display = 'none';
+          loadCalendarApi();
+        } else {
+          // Show auth UI, allowing the user to initiate authorization by
+          // clicking authorize button.
+          authorizeDiv.style.display = 'inline';
         }
+      }
 
-        $.each(data.feed.entry, function (i, el) {
+      /**
+       * Initiate auth flow in response to user clicking authorize button.
+       *
+       * @param {Event} event Button click event.
+       */
+      function handleAuthClick(event) {
+        gapi.auth.authorize(
+          {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
+          handleAuthResult);
+        return false;
+      }
 
-            var entry = $(el),
-                title = entry[0].title.$t,
-                summary = entry[0].summary.$t;
-			var when = new Date("1990-01-01");
-			
-			// Format is "When: Sat 12 Sep 2015<br>\n\n\n<br>Event Status: confirmed"
-			var sum = summary.split("<br>");
-			for (var sumIndex= 0; sumIndex<sum.length; sumIndex++) {
-				var entry= sum[sumIndex].split(":");
-				if (entry[0] == "When") {
-					when = new Date(entry[1].trim());
-				}
-			}
+      /**
+       * Load Google Calendar client library. List upcoming events
+       * once client library is loaded.
+       */
+      function loadCalendarApi() {
+        gapi.client.load('calendar', 'v3', listUpcomingEvents);
+      }
 
-            foundevents = true;
-
-            if (nextevent) {
-                if (i === 0) {
-                    nextevent.appendChild(document.createTextNode(when.toLocaleDateString("en-GB") + " : "));
-                    nextevent.appendChild(document.createTextNode(title));
-                    nextevent.appendChild(document.createElement("br"));
-                }
-            }
-
-            if (allevents) {
-                allevents.appendChild(document.createTextNode(when.toLocaleDateString("en-GB") + " : "));
-                allevents.appendChild(document.createTextNode(title));
-                allevents.appendChild(document.createElement("br"));
-                allevents.appendChild(document.createElement("br"));
-            }
+      /**
+       * Print the summary and start datetime/date of the next ten events in
+       * the authorized user's calendar. If no events are found an
+       * appropriate message is printed.
+       */
+      function listUpcomingEvents() {
+        var request = gapi.client.calendar.events.list({
+          //'calendarId': 'primary',
+		  'calendarId': '5hstrrs3i19g9untj4h7tag7d0@group.calendar.google.com',
+          'timeMin': (new Date()).toISOString(),
+          'showDeleted': false,
+          'singleEvents': true,
+          'maxResults': 20,
+          'orderBy': 'startTime'
         });
-    })
-    .fail(function () {
-        pagemessage.innerText = "No dates found.";
 
-    });
+        request.execute(function(resp) {
+		
+          document.getElementById('tc-pagemessage').style.display = 'none';
+		
+          var events = resp.items;
 
-}
+          if (events.length > 0) {
+            for (i = 0; i < events.length; i++) {
+              var event = events[i];
+              var when = event.start.dateTime;
+              if (!when) {
+                when = event.start.date;
+              }
+              appendMsg(new Date(when).toLocaleDateString('en-GB') + ' : ' + event.summary)
+            }
+          } else {
+            appendMsg('No upcoming events.');
+          }
 
+        });
+      }
+
+      /**
+       * Append element to the body containing the given message
+       * as its text node.
+       *
+       * @param {string} message Text to be placed in element.
+       */
+      function appendMsg(message) {
+        var msg = document.getElementById('tc-allevents');
+		msg.appendChild(document.createTextNode(message));
+		msg.appendChild(document.createElement("br"));
+		msg.appendChild(document.createElement("br"));
+      }
